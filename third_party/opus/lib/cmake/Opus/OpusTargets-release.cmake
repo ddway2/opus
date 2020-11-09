c1892a1a9739ce62ceabf8778d3256b5d6217dfe
Pierre Salmon <pierre.salmon@smartcontext.fr>
2020-11-09 10:11:11 +0100

    Use libopus.a built with MinGW

--- third_party/opus/lib/cmake/Opus/OpusTargets-release.cmake
+++ third_party/opus/lib/cmake/Opus/OpusTargets-release.cmake
@@ -9,11 +9,11 @@ set(CMAKE_IMPORT_FILE_VERSION 1)
 set_property(TARGET Opus::opus APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
 set_target_properties(Opus::opus PROPERTIES
   IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "C"
-  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/opus.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libopus.a"
   )
 
 list(APPEND _IMPORT_CHECK_TARGETS Opus::opus )
-list(APPEND _IMPORT_CHECK_FILES_FOR_Opus::opus "${_IMPORT_PREFIX}/lib/opus.lib" )
+list(APPEND _IMPORT_CHECK_FILES_FOR_Opus::opus "${_IMPORT_PREFIX}/lib/libopus.a" )
 
 # Commands beyond this point should not need to know the version.
 set(CMAKE_IMPORT_FILE_VERSION)
